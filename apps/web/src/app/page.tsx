197ffcf791097a3f10fe4b7b58511d9b57df6d77
Thulasiraman <thulasirmn8@gmail.com>
2024-04-06 17:45:31 +0530

    added nx

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
+import {add} from '@foundation/sample-lib';
 
 export default function Home() {
+  console.log('add', add(1, 2));
   return (
    <main>
     Hello Worldß
